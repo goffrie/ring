88561e3237b8bc822ef26d47e2a44b1db21b4350
Geoffry Song <goffrie@gmail.com>
2024-03-19 06:46:51 -0700

    allow cast_possible_truncation

--- src/rust_crypto/aes/soft.rs
+++ src/rust_crypto/aes/soft.rs
@@ -1,3 +1,5 @@
+#![allow(clippy::cast_possible_truncation)]
+
 #[cfg_attr(not(target_pointer_width = "64"), path = "soft/fixslice32.rs")]
 #[cfg_attr(target_pointer_width = "64", path = "soft/fixslice64.rs")]
 pub(crate) mod fixslice;
